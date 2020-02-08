0944161391f4f39c6841a74a11a11c3b857c2045
Daniel Chou <dchou234@gmail.com>
2020-02-08 12:26:04 -0600

    Added targeting system
    not sure where we want to be able to place turrets but fairly universal (just needs collider)

--- CityBuilder_prototype/Assets/Scripts/Enemy.cs
+++ CityBuilder_prototype/Assets/Scripts/Enemy.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    private float speed = 0.15f;
+    private float speed = 0.05f;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(0f,0f, speed);
-        if(transform.position.z > 30)
+        if(transform.position.z > 10)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -50f);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
         }
     }
 }
