e91b95ab674e8753c45efd71aa5524f66741858f
Daniel Chou <dchou234@gmail.com>
2020-02-12 16:31:23 -0600

    Scene test
    created simple scene file,
    edited enemy to give basic health system

--- Consolidated/Assets/Scripts/Enemy.cs
+++ Consolidated/Assets/Scripts/Enemy.cs
@@ -9,17 +9,38 @@ public class Enemy : MonoBehaviour
     // Pull destinations from global waypoints array
     private float dest_x = 0f;
     private float dest_y = 0f;
-    
+
+    public float health = 100f;
+    //private float speed = 0.05f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(dest_x, dest_y, speed);
-        if(transform.position.z > 30)
-        transform.Translate(0f,0f, speed);
+        transform.Translate(-0.01f,0, -0.01f);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        //transform.Translate(dest_x, dest_y, speed);
+        //transform.position = new Vector3(transform.position.x, transform.position.y, 0.01f);
+        //if(transform.position.z > 30)
+        //transform.Translate(0f,0f, speed);
+        /*
         if(transform.position.z > 10)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+        }*/
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            health -= 20;
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 }
---
+++ Consolidated/Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("SpawnNext", 10f, Random.Range(3f, 5f));
+    }
+
+    void Awake(){
+        Invoke("SpawnNext", 5f);
+    }
+    void SpawnNext(){
+        
+        GameObject new_enemy = Instantiate(Sphere, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //GameObject new_enemy = Instantiate(Sphere);
+    }
+}
